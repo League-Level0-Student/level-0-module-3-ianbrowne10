5319687138a70a91b1dfbfb7f8f407ad1dc62d1e
ianbrowne10 <League@administorsimac.attlocal.net>
2017-12-28 14:32:18 -0800

    CrazyCatLady, HighLowGame (IP)

--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -10,20 +10,27 @@ import javax.swing.JOptionPane;
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-String Cats = JOptionPane.showInputDialog("How many cats do you have?");
-		
-// 2. Convert their answer into an int
-	int Kittens = Integer.parseInt(Cats); 
-		// 3. If they have more than 3 cats, tell them they're a crazy cat lady
-if(Kittens>3) {
-	JOptionPane.showMessageDialog(null, "You're a CRAZY CAT LADY!");
-}
-		// 4. If they have 3 or less, call the method below to show them a cat video
- if(Kittens<3) {
+		String Cats = JOptionPane.showInputDialog("How many cats do you have?");
 
- }
-		// 5. If they say 0, show them a video of A Frog Sitting on a Bench Like a Human
+		// 2. Convert their answer into an int
+		int Kittens = Integer.parseInt(Cats);
+		// 3. If they have more than 3 cats, tell them they're a crazy cat lady
+		if (Kittens > 3) {
+			JOptionPane.showMessageDialog(null, "You're a CRAZY CAT LADY!");
+		}
+		// 4. If they have 3 or less, call the method below to show them a cat video
+		else if(Kittens == 3) {
+			
+		}
 		
+		else if (Kittens < 3 && Kittens > 0 ) {
+			playVideo("https://www.youtube.com/watch?v=dGFSjKuJfrI");
+		}
+
+		// 5. If they say 0, show them a video of A Frog Sitting on a Bench Like a Human
+		else if (Kittens == 0) {
+			playVideo("https://www.youtube.com/watch?v=ZJT9CeEhM10");
+		}
 	}
 
 	static void playVideo(String videoURL) {
@@ -36,4 +43,3 @@ if(Kittens>3) {
 	}
 
 }
-
--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -5,27 +5,43 @@ package elseif;
 
 import java.util.Random;
 
+import javax.print.attribute.standard.PrinterLocation;
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		int random = new Random().nextInt(99);
 		// 2. Print out the random variable above
 		
+	System.out.println(random);
 		// 11. do the following 10 times
 		
 			// 1. ask the user for a guess using a pop-up window, and save their response 
-
+	String Number = JOptionPane.showInputDialog("Guess a number (1-100)");
 			// 4. convert the users’ answer to an int (Integer.parseInt(string))
-			
+			int answer  = (Integer.parseInt(Number));
 			// 5. if the guess is correct
-				// 6. win
+				if(answer == random) {
+					JOptionPane.showMessageDialog(null, "You Won!");
+				}
+			// 6. win
 			// 7. if the guess is high
+				if(answer > random) {
+					JOptionPane.showMessageDialog(null, "You're too high~!");
+				}
 				// 8. tell them it's too high
-			// 9. if the guess is low
+			
+				// 9. if the guess is low
 				// 10. tell them it's too low
 
 		// 12. tell them they lose
+	}
+
+	private static void println(int random) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
