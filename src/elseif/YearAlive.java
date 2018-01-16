f26e4779dc4a36a54abee401af44559280afff56
ianbrowne10 <league@WorthyMini-6.attlocal.net>
2018-01-15 18:24:19 -0800

    Shiny Objects, Years Alive, Walk of Fame

---
+++ src/elseif/YearAlive.java
@@ -0,0 +1,17 @@
+package elseif;
+
+public class YearAlive {
+public static void main(String[] args) {
+	
+	for (int i = 2006; i < 2019 ; i++) {
+		System.out.println(i);
+	}
+		
+	
+
+
+
+
+
+}
+}
--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -8,21 +8,42 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
 	public static void main(String[] args) {
-		Robot rob = new Robot();
+		Robot mini = new Robot("mini");
 		// 1. Set the X position of the robot so that it starts on the left. 
-		// You also need to show the robot to see the result of this line.
+	
+	// You also need to show the robot to see the result of this line.
 
-		// 2. Make the robot draw a star shape. Hint: 144.
+		mini.penDown();
+		mini.setAngle(90);
+mini.setSpeed(10);
+// 2. Make the robot draw a star shape. Hint: 144.
+for(int i = 0; i < 5; i++) {
+for (int i2 = 0; i2 < 6; i2++) {
+	
+mini.penDown();
+mini.turn(144);
+		mini.move(30);
+}
+mini.setAngle(-90);
+mini.penUp();
 
-		// 3. Set the size of the star to 30.
+mini.move(80ss);
+}
+
+
+
+
+
+
+
+// 3. Set the size of the star to 30.
 
 		/** THE CHALLENGE: **/
 		/*
 		 * Make the robot draw a line of stars like this:
-		 * http://bit.ly/walk-of-fame
-		 * 
-		 * Hint: The distance between stars is 50.
 		 */
+		
+
 
 	}
 
