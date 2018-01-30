3b85c5fa0e910ecc50e72cb6c8fb6233ea7478c3
J.D.S. Solutions <league@WTS19.attlocal.net>
2018-01-29 18:27:00 -0800

    ObedientRobot(IP)

--- src/elseif/ObediantRobot.java
+++ src/elseif/ObediantRobot.java
@@ -1,25 +1,65 @@
 package elseif;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObediantRobot {
 public static void main(String[] args) {
-
-
-drawSquare(4);
-
+	String answer = JOptionPane.showInputDialog("What shape do you want? (Circle, Square, Triangle)");
+if(answer.equalsIgnoreCase("square")) {
+	drawSquare();
+}
+if(answer.equalsIgnoreCase("triangle")) {
+	drawTriangle();
+}
+if(answer.equalsIgnoreCase("circle")) {
+	drawCircle();
 }
 
+}
+String answer = JOptionPane.showInputDialog("What shape do you want? (Circle, Square, Triangle)");
 
-private static void drawSquare(int length) {
-	Robot I = new Robot();
-	I.penDown();
-	I.setSpeed(10);
-	I.hide();
+
+
+private static void drawSquare() {
+	Robot i = new Robot();
+i.penDown();
+	
+i.penDown();
+	i.setSpeed(1000);
+	i.hide();
 	for (int j = 0; j < 4; j++) {
 	
-		I.move(100);
-	I.turn(90);
+		i.move(100);
+	i.turn(90);
+}}
+private static void drawTriangle() {
+	 Robot andrew = new Robot();
+andrew.penDown();
+andrew.setSpeed(1000);
+andrew.hide();
+
+for (int y =0; y<3; y++) {
+	andrew.turn(120);
+	
+andrew.move(100);
+}
+}
+private static void drawCircle() {
+	Robot bill = new Robot();
+			bill.setSpeed(1000);
+bill.hide();
+bill.penDown();
+for (int e = 0; e<360; e++) {
+	bill.setAngle(e);
+	
+bill.move(1);
+}
 }
 
-}}
+
+
+
+
+}
